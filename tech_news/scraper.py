90ac800f7702ef1e2b2178b5429878a7fa476c4d
Mathews <mathewsrodrigues9@gmail.com>
2023-03-13 15:19:58 -0300

    criando função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,38 @@
+# from bs4 import BeautifulSoup
+import requests
+import time
+
+
+headers = {"User-Agent": "Fake user-agent"}
+base_url = "https://blog.betrybe.com/"
+
+
 # Requisito 1
-def fetch(url):
-    """Seu código deve vir aqui"""
+def fetch(url: str, wait: int = 1):
+    try:
+        response = requests.get(url, timeout=wait, headers=headers)
+        time.sleep(1)
+        response.raise_for_status()
+    except (requests.HTTPError, requests.ReadTimeout):
+        return None
+    return response.text
 
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    pass
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    pass
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    pass
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    pass
