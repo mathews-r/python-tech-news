02d5efbbc61bf7c0e61472456f5c30f0e01615f4
Mathews <mathewsrodrigues9@gmail.com>
2023-03-13 16:01:37 -0300

    criando função scrape_updates

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,4 +1,4 @@
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
 import time
 
@@ -13,14 +13,21 @@ def fetch(url: str):
         response = requests.get(url, headers=headers)
         time.sleep(1)
         response.raise_for_status()
+
     except (requests.HTTPError, requests.ReadTimeout):
         return None
+
     return response.text
 
 
 # Requisito 2
 def scrape_updates(html_content):
-    pass
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    news_list = []
+    for news in soup.find_all("a", class_="cs-overlay-link"):
+        news_list.append(news.get("href"))
+    return news_list
 
 
 # Requisito 3
@@ -36,3 +43,7 @@ def scrape_news(html_content):
 # Requisito 5
 def get_tech_news(amount):
     pass
+
+
+html = fetch(base_url)
+scrape_updates(html)
