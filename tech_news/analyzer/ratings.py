34787383a87af928f98b10c7ef8c1aef7ece9315
Mathews <mathewsrodrigues9@gmail.com>
2023-03-14 16:09:30 -0300

    concluindo funcao top_5

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from collections import Counter
 
 
 # Requisito 10
@@ -7,12 +8,15 @@ def top_5_categories():
 
     categories = []
     for category in news:
-        if category["category"] not in news:
-            categories.append(category["category"])
-        else:
-            True
-    print(categories)
-    return categories
+        categories.append(category["category"])
+
+    sorted_categories = Counter(sorted(categories)).most_common()
+
+    new_categories = []
+    for category in sorted_categories[0:5]:
+        new_categories.append(category[0])
+
+    return new_categories
 
 
 top_5_categories()
